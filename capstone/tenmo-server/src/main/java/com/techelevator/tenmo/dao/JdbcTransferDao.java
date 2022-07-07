a5040b5e7f8352b118d2a387287596f508ec6465
Mike Still <mikestill12@gmail.com>
2022-07-07 10:36:29 -0400

    Day 2, trying to login

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -1,4 +1,56 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.Transfer;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class JdbcTransferDao implements TransferDao{
+
+        private JdbcTemplate jdbcTemplate;
+
+        public JdbcTransferDao(JdbcTemplate jdbcTemplate) {
+            this.jdbcTemplate = jdbcTemplate;
+        }
+
+    @Override
+    public List<Transfer> findAll() {
+        return null;
+    }
+
+    @Override
+    public Transfer findByTransferID(int id) {
+        return null;
+    }
+
+    @Override
+    public Transfer findByTransferType(int id) {
+        return null;
+    }
+
+    @Override
+    public Transfer findByStatusId(int id) {
+        return null;
+    }
+
+    @Override
+    public Transfer findByTransferStatusDesc(String status) {
+        return null;
+    }
+
+    @Override
+    public Transfer findByAccountFrom(int id) {
+        return null;
+    }
+
+    @Override
+    public Transfer findByAccountTo(int id) {
+        return null;
+    }
+
+    @Override
+    public Transfer getTransferAmount(BigDecimal transferBalance) {
+        return null;
+    }
 }
--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -1,4 +1,30 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.Account;
+import com.techelevator.tenmo.model.Transfer;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface TransferDao {
+
+    List<Transfer> findAll();
+
+    Transfer findByTransferID(int id);
+
+    Transfer findByTransferType(int id);
+
+    Transfer findByStatusId(int id);
+
+    Transfer findByTransferStatusDesc(String status);
+
+    Transfer findByAccountFrom(int id);
+
+    Transfer findByAccountTo(int id);
+
+    Transfer getTransferAmount(BigDecimal transferBalance);
+
+
+
+
 }
